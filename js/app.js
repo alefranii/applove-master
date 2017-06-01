153e23b6f89b3231999dfa56b07532be0b7bec71
alefranii <afmormar@gmail.com>
2017-06-01 02:19:05 -0400

    funciona pero mejorar barra

--- js/app.js
+++ js/app.js
@@ -1,3 +1,32 @@
-/*
- * Archivo principal de funcionalidad de JS
- */
+
+ (function main(){
+ 	var boxes = Array.from(document.getElementsByClassName("imagenes"));
+ 	var work = document.getElementById("work");
+
+ 	var modal;
+ 	var bodyModal;
+ 	var close;
+ 	var imagen;
+ 	boxes.forEach(function(box){
+ 		box.addEventListener("click", function(){
+ 			modal = document.createElement("div");
+ 			modal.classList.add("box-services-modal");
+ 			bodyModal = document.createElement("div");
+ 			bodyModal.classList.add("modal-body");
+ 			bodyModal.innerHTML = box.innerHTML;			
+ 			modal.appendChild(bodyModal);
+
+ 			close = document.createElement("div");
+ 			close.classList.add("close");
+ 			imagen = document.createElement("img");
+ 			imagen.setAttribute("src", "http://tojaeurope.com/gallery/Close-icon.png");
+ 			close.appendChild(imagen);
+ 			modal.appendChild(close);
+ 			work.appendChild(modal);
+ 			close.addEventListener("click",function(){
+ 				modal.classList.add("hide");
+ 				work.removeChild(modal);
+ 			});
+ 		});		
+ 	});
+ })();
